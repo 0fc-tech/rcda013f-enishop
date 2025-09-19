678dae633939a489e13101dc3e76ee5982ebc70f
qrouet <quentinrouet@hotmail.fr>
2025-09-19 09:30:28 +0200

    Dernière corection

--- app/src/main/java/com/example/enishop/MainActivity.kt
+++ app/src/main/java/com/example/enishop/MainActivity.kt
@@ -34,36 +34,36 @@ import com.example.enishop.ui.theme.EniShopTheme
 import com.example.enishop.ui.theme.Typography
 import java.time.LocalDateTime
 import java.util.Date
-
-class MainActivity : ComponentActivity() {
-    val articles = listOf(
-        Article(
-            id = 1,
-            name = "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-            description = "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-            price = 109.95f,
-            urlImage = "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
-            category = "men's clothing",
-            date = Date(),
-        ),
-        Article(
-            id = 2,
-            name = "Mens Casual Premium Slim Fit T-Shirts",
-            description = "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-            price = 22.3f,
-            urlImage = "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png",
-            category = "men's clothing",
-            date = Date(),
-        ),
-        Article(
-            id = 3,
-            name = "Mens Cotton Jacket",
-            description = "great outerwear jackets for Spring/Autumn/Winter, suitablefor many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
-                price = 55.99f,
-            urlImage = "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png",
-            category = "men's clothing", date = Date()
-        )
+val articles = listOf(
+    Article(
+        id = 1,
+        name = "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+        description = "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        price = 109.95f,
+        urlImage = "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+        category = "men's clothing",
+        date = Date(),
+    ),
+    Article(
+        id = 2,
+        name = "Mens Casual Premium Slim Fit T-Shirts",
+        description = "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+        price = 22.3f,
+        urlImage = "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png",
+        category = "men's clothing",
+        date = Date(),
+    ),
+    Article(
+        id = 3,
+        name = "Mens Cotton Jacket",
+        description = "great outerwear jackets for Spring/Autumn/Winter, suitablefor many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+        price = 55.99f,
+        urlImage = "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png",
+        category = "men's clothing", date = Date()
     )
+)
+class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -79,35 +79,7 @@ class MainActivity : ComponentActivity() {
                         }
                     )
                 },) { innerPadding ->
-                    Column(Modifier
-                        .padding(innerPadding)
-                        .padding(16.dp)) {
-                        Text(articles[0].name,
-                            style = Typography.headlineMedium)
-                        AsyncImage(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(250.dp),
-                            model=articles[0].urlImage,
-                            contentDescription = articles[0].name)
-                        Text(articles[0].description)
-                        Row(modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceBetween) {
-                            Text("Prix: ${articles[0].price}€")
-                            Text("Date de sortie : ${articles[0].date.date}/${articles[0].date.month+1}")
-                        }
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.Center) {
-                            Checkbox(
-                                true,
-                                onCheckedChange = null,
-                            )
-                            Text("Favoris? : ")
-                        }
-
-
-                    }
+                    DetailProduit(Modifier.padding(innerPadding))
                 }
             }
         }
@@ -115,3 +87,37 @@ class MainActivity : ComponentActivity() {
 
 }
 
+@Composable
+fun DetailProduit(modifier: Modifier = Modifier) {
+    var isFavorite = false;
+
+    Column(modifier
+        .padding(16.dp)) {
+        Text(articles[0].name,
+            style = Typography.headlineMedium)
+        AsyncImage(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(250.dp),
+            model=articles[0].urlImage,
+            contentDescription = articles[0].name)
+        Text(articles[0].description)
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween) {
+            Text("Prix: ${articles[0].price}€")
+            Text("Date de sortie : ${articles[0].date.date}/${articles[0].date.month+1}")
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center) {
+            Checkbox(
+                isFavorite,
+                onCheckedChange = { checked -> isFavorite = checked },
+            )
+            Text("Favoris?  ")
+        }
+
+
+    }
+}
